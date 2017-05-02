1b6af18f5b1d2dc0a86ebb53c8fecada85e4c4d5
MrGullman <jesper.gullman@outlook.com>
2017-05-02 15:53:24 +0200

    v
    v

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 $(document).on("pagecreate", "#pageone", function(){
 
-    $(".view").on('swiperight', swiperightHandeler);
-    $(".view").on('swipeleft', swipeleftHandeler);
-
-    function swiperightHandeler() {
-        $(this).animate({left:"150px"}, 400);
-        $(".buttons").animate({left:"0px"}, 400);
-      //$(this).addClass("swiperight");
-    }
-
-    function swipeleftHandeler() {
-        $(this).animate({left:"0px"}, 400);
-        $(".buttons").animate({left:"-150px"}, 400);
-      //$(this).removeClass("swiperight");
-    }
-
-    $(document).on('swipeleft', 'li', function(){
-      console.log("swipe left");
+    $("p").on("swiperight",function(){
+        $(this).animate({left: "150px"}, 400);
     });
 
-    $(document).on('swiperight', 'li', function(){
-      console.log("swipe right");
+    $("p").on("swipeleft",function(){
+        $(this).animate({left: "0px"}, 400);
     });
+
+    // $(".view").on('swiperight', swiperightHandeler);
+    // $(".view").on('swipeleft', swipeleftHandeler);
+    //
+    // function swiperightHandeler() {
+    //     $(this).animate({left:"150px"}, 400);
+    //     $(".buttons").animate({left:"0px"}, 400);
+    //   //$(this).addClass("swiperight");
+    // }
+    //
+    // function swipeleftHandeler() {
+    //     $(this).animate({left:"0px"}, 400);
+    //     $(".buttons").animate({left:"-150px"}, 400);
+    //   //$(this).removeClass("swiperight");
+    // }
+    //
+    // $(document).on('swipeleft', 'li', function(){
+    //   console.log("swipe left");
+    // });
+    //
+    // $(document).on('swiperight', 'li', function(){
+    //   console.log("swipe right");
+    // });
 
 
 });
